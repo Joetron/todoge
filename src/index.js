bd684e80a33b46f7faa3a4db1a122a611fb32798
joetron <joetron@hotmail.com>
2017-05-12 23:14:37 -0700

    Fixed bugs in css refactor from SASS to pure CSS, mainly the hover selector and before psuedo element for the edit and close icons

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-import './index.css';
-
-//ReactDOM.render(
- // <App />,
- // document.getElementById('root')
-//);
-
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
 import TodoApp from './components/TodoApp.jsx'
+import './index.css'
 
 let store = createStore(todoApp)
 
