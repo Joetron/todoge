851efe00adca1d926b1c93413c14f3c4b159084d
joetron <joetron@hotmail.com>
2017-05-12 17:11:04 -0700

    added a util class that has functions to read and write to session storage, create a link for the current todo list, and read the props from a query to populate a todolist from a link.

---
+++ src/actions/index.js
@@ -0,0 +1,80 @@
+import ActionTypes from '../constants/ActionTypes.js';
+import uuidV1 from 'uuid/v1';
+
+function addTodo(text) {
+	return {
+		type: ActionTypes.ADD_TODO,
+		payload: {
+			text,
+			id: uuidV1()
+		}
+	};
+}
+
+function editTodo(id) {
+	return {
+		type: ActionTypes.EDIT_TODO,
+		payload: {
+			id
+		}
+	};
+}
+
+function cancelUpdate(id) {
+	return {
+		type: ActionTypes.CANCEL_EDIT,
+		payload: {
+			id
+		}
+	};
+}
+
+function updateTodo(id, text) {
+	return {
+		type: ActionTypes.UPDATE_TODO,
+		payload: {
+			id, text
+		}
+	};
+}
+
+function deleteTodo(id) {
+	return {
+		type: ActionTypes.DELETE_TODO,
+		payload: {
+			id
+		}
+	};
+}
+
+function toggleComplete(id) {
+	return {
+		type: ActionTypes.TOGGLE_COMPLETED,
+		payload: {
+			id
+		}
+	};
+}
+
+function toggleVisibility(filter) {
+	return {
+		type: ActionTypes.TOGGLE_VISIBILITY,
+		payload: {
+			filter
+		}
+	};
+}
+
+function shareLink() {
+	return {
+		type: ActionTypes.SHOW_SHARE
+	};
+}
+
+function hideLink() {
+	return {
+		type: ActionTypes.HIDE_SHARE
+	};
+}
+
+export { addTodo, editTodo, cancelUpdate, updateTodo, deleteTodo, toggleComplete, toggleVisibility, shareLink, hideLink };
