86f15bcd0a0c6e4071a2a9416f4f52c2e3b98fd0
joetron <joetron@hotmail.com>
2017-05-12 22:26:14 -0700

    Renamed todo to todos it's to proper name. Cleaned up index.js entry point and included css file.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+//ReactDOM.render(
+ // <App />,
+ // document.getElementById('root')
+//);
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import TodoApp from './components/TodoApp.jsx'
+
+let store = createStore(todoApp)
+
+render(
+	<Provider store={store}>
+		<TodoApp />
+	</Provider>,
+	document.getElementById('root')
+)
