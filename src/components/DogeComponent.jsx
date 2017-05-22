5d508fe944064284514ec31e6bf8eccec373e5ce
joetron <joetron@hotmail.com>
2017-05-22 16:30:39 -0700

    Added a component for rendering a doge meme that will be generated from the todos. Currently populated with static text. TODOGE: create tests, create container, modify width and style.

---
+++ src/components/DogeComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const DogeComponent = ( ) => (
+	<div className="doge-image" >
+		<div className="doge-row-top" >
+			<div className="doge-row-top-left" >
+				<span className="doge-row-top-text doge-text-one">Very todo</span>
+				<span className="doge-row-top-text doge-text-two">Such words</span>
+			</div>
+			<div className="doge-row-top-right">
+				<div className="doge-row-top-right-left">
+					<span className="doge-row-bottom-text doge-text-five">Dare type</span>
+				</div>
+				<div className="doge-row-top-right-right">
+					<span className="doge-row-bottom-text doge-text-six">Advise</span>
+				</div>
+			</div>
+		</div>
+		<div className="doge-row-bottom" >
+			<span className="doge-row-bottom-text doge-text-three">Much app</span>
+			<span className="doge-row-bottom-text doge-text-four">So work</span>
+		</div>
+	</div>
+)
+
+export default DogeComponent
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -3,6 +3,7 @@ import AddTodoContainer from '../containers/AddTodoContainer.js'
 import TodosContainer from '../containers/TodosContainer.js'
 import Header from './parts/Header.jsx';
 import Sidebar from './parts/Sidebar.jsx'
+import DogeComponent from './DogeComponent.jsx'
 import FilterContainer from '../containers/FilterContainer.js'
 import ShareTodosContainer from '../containers/ShareTodosContainer.js'
 
@@ -12,7 +13,7 @@ const TodoApp = () => (
 		<div className="middle" >
 			<div className="todo-wrapper" >
 				<Header>
-					<div>Todos</div>
+					<DogeComponent />
 				</Header>
 				<AddTodoContainer />
 				<TodosContainer />
